553c81effe0ddad9440e81fb7ff078e48ac0add0
Denilson Bonatti <denilsonbonatti@gmail.com>
2023-09-23 14:04:27 +0000

    Update file server.test.js

--- app/server.test.js
+++ app/server.test.js
@@ -6,6 +6,11 @@ test('main index.html file exists', () => {
   expect(fs.existsSync(filePath)).toBeTruthy();
 });
 
+test('main xxx.html file exists', () => {
+  const filePath = path.join(__dirname, "index.html")
+  expect(fs.existsSync(filePath)).toBeTruthy();
+});
+
 test('Dockerfile exists', () => {
   const filePath = path.join(__dirname, "..", "Dockerfile")
   expect(fs.existsSync(filePath)).toBeTruthy();
